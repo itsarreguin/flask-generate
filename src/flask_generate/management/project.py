2a4d0fa923d16afc79e4292034d722d7bd03f5bc
Brandon Arreguin <arreguin.tech@gmail.com>
2026-02-11 12:45:24 -0600

    App creation command added

---
+++ src/flask_generate/management/project.py
@@ -0,0 +1,24 @@
+import click
+
+from .._files import create_mvc_app_structure
+from .._files import create_blueprint_app_structure
+
+
+@click.group
+def cli():
+    """ Base function for flask-generate commads. """
+
+
+@cli.command()
+@click.option('--name', help='App name')
+@click.option('--type', help='App structure type', default='mvc')
+@click.option('--orm', help='Choice an ORM: SQLAlchemy/Peewee', default='sqla')
+def app(name: str, type: str, orm: str):
+    if type == 'blueprint':
+        create_blueprint_app_structure(name, orm)
+    else:
+        create_mvc_app_structure(name, orm)
+
+
+def execute_cli():
+    cli()
